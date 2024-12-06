580452cc3c9ae70de81b650534f10e9b1295eecf
Taichi Ogawa <ta1ogw0970@outlook.jp>
2024-12-06 17:57:48 +0900

    lint修正

--- app/auth/login/LoginForm.tsx
+++ app/auth/login/LoginForm.tsx
@@ -69,7 +69,7 @@ const LoginForm = () => {
             } else {
                 console.log("ユーザー情報の取得に失敗しました")
             }
-        } catch (error: any) {
+        } catch (error) {
             console.log(error)
             setEmailError(true)
             setPasswordError(true)
